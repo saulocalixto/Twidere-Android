e4c1b91ba55dc9f3ce55eca5b086b87357f29fa0
Saulo Calixto <sauloacalixto@gmail.com>
2018-09-26 22:14:58 -0300

    Refatoração.

--- twidere/src/main/java/WarcraftApi.java
+++ twidere/src/main/java/WarcraftApi.java
@@ -9,8 +9,9 @@ import java.util.Scanner;
 
 public class WarcraftApi {
 
-    private static final String API_KEY = "awydkuy9tx3zksd7kkh2g5xchnwv4qy3";
-    private static final String BASE_URL = "https://us.api.battle.net/wow/";
+    private final String API_KEY = "awydkuy9tx3zksd7kkh2g5xchnwv4qy3";
+    private final String BASE_URL = "https://us.api.battle.net/wow/";
+    private StringBuilder stringBuilder;
 
     /**
      * Gets all the recent achievements of the specified guild in a JSON format (inside of a String)
@@ -20,7 +21,7 @@ public class WarcraftApi {
      * @return
      * @throws IOException
      */
-    public static String getGuildAchievements(String server, String guild, ApiLocale locale)
+    public String getGuildAchievements(String server, String guild, ApiLocale locale)
             throws IOException {
         URL url = new URL(getGuildApiLink("achievements", server, guild, locale));
         return getJson(url);
@@ -34,7 +35,7 @@ public class WarcraftApi {
      * @return
      * @throws IOException
      */
-    public static String getGuildNews(String server, String guild, ApiLocale locale)
+    public String getGuildNews(String server, String guild, ApiLocale locale)
             throws IOException {
         URL url = new URL(getGuildApiLink("news", server, guild, locale));
         return getJson(url);
@@ -48,23 +49,22 @@ public class WarcraftApi {
      * @return
      * @throws IOException
      */
-    public static String getAchievement(String id, ApiLocale locale) throws IOException {
+    public String getAchievement(String id, ApiLocale locale) throws IOException {
         return getJson(new URL(getAchievementApiLink(id, locale)));
     }
 
-    private static String getAchievementApiLink(String id, ApiLocale locale) {
+    private String getAchievementApiLink(String id, ApiLocale locale) {
         ApiLocale responseLocale = parseLocale(locale);
 
-        StringBuilder builder = new StringBuilder();
-        builder.append(BASE_URL);
-        builder.append("achievement/");
-        builder.append(id);
-        builder.append("&locale=");
-        builder.append(responseLocale);
-        builder.append("&apikey=");
-        builder.append(API_KEY);
-
-        return  builder.toString();
+        return getStringBuilder()
+            .append(BASE_URL)
+            .append("achievement/")
+            .append(id)
+            .append("&locale=")
+            .append(responseLocale)
+            .append("&apikey=")
+            .append(API_KEY)
+            .toString();
     }
 
     /**
@@ -77,26 +77,26 @@ public class WarcraftApi {
      * @param locale The language of the response.
      * @return
      */
-    private static String getGuildApiLink(String fields, String server, String guild, ApiLocale locale) {
+    private String getGuildApiLink(String fields, String server, String guild, ApiLocale locale) {
         ApiLocale responseLocale = parseLocale(locale);
 
-        StringBuilder builder = new StringBuilder();
-        builder.append(BASE_URL);
-        builder.append("guild/");
-        builder.append(server);
-        builder.append("/");
-        builder.append(guild);
-        builder.append("?fields=");
-        builder.append(fields);
-        builder.append("&locale=");
-        builder.append(responseLocale);
-        builder.append("&apikey=");
-        builder.append(API_KEY);
-        return builder.toString();
+        return getStringBuilder()
+            .append(BASE_URL)
+            .append("guild/")
+            .append(server)
+            .append("/")
+            .append(guild)
+            .append("?fields=")
+            .append(fields)
+            .append("&locale=")
+            .append(responseLocale)
+            .append("&apikey=")
+            .append(API_KEY)
+            .toString();
     }
 
-    private static String getJson(URL url) throws IOException {
-        StringBuilder file = new StringBuilder();
+    private String getJson(URL url) throws IOException {
+        StringBuilder file = getStringBuilder();
         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
@@ -113,7 +113,7 @@ public class WarcraftApi {
         return file.toString();
     }
 
-    private static ApiLocale parseLocale(ApiLocale locale) {
+    private ApiLocale parseLocale(ApiLocale locale) {
         ApiLocale responseLocale = locale;
         if (responseLocale == null)
             responseLocale = ApiLocale.en_US;
@@ -121,10 +121,16 @@ public class WarcraftApi {
         return responseLocale;
     }
 
+    private static StringBuilder getStringBuilder() {
+        return stringBuilder != null
+                ? stringBuilder
+                : (stringBuilder = new StringBuilder());
+    }
+
     /**
      * All available languages for Battle.net API responses
      */
-    public static enum ApiLocale {
+    public enum ApiLocale {
         en_US, pt_BR, es_MX;
     }
 }
